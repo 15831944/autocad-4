a47fbdda45361807981e669d1778535e15e93afc
mirko <mirkomaga@virgilio.it>
2020-07-06 18:10:41 +0200

    Aggiunta verifica numerica

--- autocad/UserControl1.Designer.cs
+++ autocad/UserControl1.Designer.cs
@@ -28,22 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Lunghezza_input = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Spessore_input = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox2
+            // Lunghezza_input
             // 
-            this.textBox2.AccessibleDescription = "L";
-            this.textBox2.AccessibleName = "L";
-            this.textBox2.Location = new System.Drawing.Point(107, 24);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(30, 20);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.Lunghezza_input.AccessibleDescription = "L";
+            this.Lunghezza_input.AccessibleName = "L";
+            this.Lunghezza_input.Location = new System.Drawing.Point(111, 66);
+            this.Lunghezza_input.Name = "Lunghezza_input";
+            this.Lunghezza_input.Size = new System.Drawing.Size(30, 20);
+            this.Lunghezza_input.TabIndex = 4;
+            this.Lunghezza_input.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // pictureBox1
             // 
@@ -53,6 +53,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(122, 114);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button2
             // 
@@ -64,24 +65,26 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // Spessore_input
             // 
-            this.textBox1.AccessibleDescription = "s";
-            this.textBox1.AccessibleName = "s";
-            this.textBox1.Location = new System.Drawing.Point(26, 95);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(30, 20);
-            this.textBox1.TabIndex = 8;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.Spessore_input.AccessibleDescription = "s";
+            this.Spessore_input.AccessibleName = "s";
+            this.Spessore_input.Location = new System.Drawing.Point(53, 94);
+            this.Spessore_input.Name = "Spessore_input";
+            this.Spessore_input.Size = new System.Drawing.Size(30, 20);
+            this.Spessore_input.TabIndex = 8;
+            this.Spessore_input.Tag = "Spessore";
+            this.Spessore_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Spessore_input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Spessore_input);
+            this.Controls.Add(this.Lunghezza_input);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(350, 295);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -91,9 +94,9 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Lunghezza_input;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Spessore_input;
     }
 }
--- autocad/UserControl1.cs
+++ autocad/UserControl1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Autodesk.Private.InfoCenter;
 
 namespace autocad
 {
     public partial class UserControl1 : UserControl
     {
+
         public UserControl1()
         {
             InitializeComponent();
@@ -20,19 +22,48 @@ namespace autocad
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             // input lunghezze
+
+            this.checkIfNumber(Lunghezza_input);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Prendo punti coordinate click
+            // Controllo che non ci siano campi vuoti e che siano numeri
+            string spessore = Spessore_input.Text;
+            string lunghezza = Lunghezza_input.Text;
 
-            Console.WriteLine(sender);
-            Console.WriteLine(e);
+            // * System.Diagnostics.Debug.WriteLine(e);
+            if (string.IsNullOrEmpty(spessore) || string.IsNullOrEmpty(lunghezza))
+            {
+                // richiedi l'inserimento del punto base
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             // input spessore
+
+            this.checkIfNumber(Spessore_input);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkIfNumber(TextBox box)
+        {
+            string tString = box.Text;
+            if (tString.Trim() == "") return;
+            for (int i = 0; i < tString.Length; i++)
+            {
+                if (!char.IsNumber(tString[i]))
+                {
+                    MessageBox.Show("inserisci un numero");
+                    box.Text = "";
+                    return;
+                }
+            }
         }
     }
 }
